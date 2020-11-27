e8c669654e3e37dc3c19aa808c4170716ba411dd
aZapata <jaalza@gmail.com>
2020-11-26 23:28:16 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app     = express();
+var server  = require('http').Server(app);
+var io      = require('socket.io').listen(server); 
+var cors = require('cors');
+const port = 8888;
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/ajax-loader.gif', function(req, res){ res.sendFile(__dirname + '/ajax-loader.gif') });
+
+server.listen(process.env.PORT || port, () =>  console.log(`Listening on ${server.address().port}`) );
